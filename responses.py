0555eba1b3289b7eaee2c2c1c10f92c49dd7e050
Tristanv0 <cheng.tristan@hotmail.com>
2023-08-03 11:06:35 -0400

    Embed Messages
    added help command
     -list of commands
    
    embed bot messages

--- responses.py
+++ responses.py
@@ -1,8 +1,16 @@
+import discord
 import random
 
-def handle_response(message) -> str: 
+def handle_response(message): 
     p_message = message.lower()
 
     if p_message == 'roll':
         return str(random.randint(1,6))
     
+    if p_message == 'help':
+        embed = discord.Embed(
+            colour=discord.Colour.dark_blue(),
+            title="List of Commands",
+            description="``roll``   |    Roll a dice!"
+        )
+        return embed
