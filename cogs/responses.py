9f8e317a6c186d1942ca0db1bb715e4bf22233c9
Tristanv0 <cheng.tristan@hotmail.com>
2023-08-03 15:23:29 -0400

    Create responses.py
    made responses.py a cog.
    will have all misc commands for now.
    Will probably rename file name later on to something more appropriate

---
+++ cogs/responses.py
@@ -0,0 +1,28 @@
+import discord
+import random
+from discord import app_commands
+from discord.ext import commands
+
+class ResponsesCog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print("Responses cog loaded")
+
+    @app_commands.command(name="roll")
+    async def roll(self, interaction: discord.Interaction):
+        """Roll a dice and get a random number between 1 and 6."""
+        random_number = random.randint(1, 12)
+        embed = discord.Embed(colour=discord.Colour.dark_red(), title="Dice", description=f"You rolled a {random_number}!")
+        await interaction.response.send_message(embed=embed)
+
+    @app_commands.command(name="help")
+    async def help(self, interaction: discord.Interaction):
+        embed = discord.Embed(colour=discord.Colour.dark_blue(), title="List of commands", description="``roll``   |    Roll a dice!")
+        await interaction.response.send_message(embed=embed)
+
+async def setup(bot):
+    await bot.add_cog(ResponsesCog(bot))
+
