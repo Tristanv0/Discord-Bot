7648f329993938d8e05e36cd9db9ba00c15e960c
cheliyan-s <127283399+cheliyan-s@users.noreply.github.com>
2023-08-06 21:30:03 -0400

    edited /roll command

--- cogs/responses.py
+++ cogs/responses.py
@@ -13,9 +13,9 @@ class ResponsesCog(commands.Cog):
 
     @app_commands.command(name="roll")
     async def roll(self, interaction: discord.Interaction):
-        """Roll a dice and get a random number between 1 and 6."""
+        """Roll a dice and get a random number between 2 and 12"""
         random_number = random.randint(2, 12)
-        embed = discord.Embed(colour=discord.Colour.dark_red(), title="Dice", description=f"You rolled a {random_number}!")
+        embed = discord.Embed(colour=discord.Colour.dark_red(), title="Dice", description=f"🎲 You rolled a {random_number}!")
         await interaction.response.send_message(embed=embed)
 
     @app_commands.command(name="help")
@@ -23,7 +23,7 @@ class ResponsesCog(commands.Cog):
         """List of all commands"""
         embed = discord.Embed(colour=discord.Colour.dark_blue(), title="List of commands", description="``roll``   |    Roll a dice!")
         await interaction.response.send_message(embed=embed)
-
+    
 async def setup(bot):
     await bot.add_cog(ResponsesCog(bot))
 
