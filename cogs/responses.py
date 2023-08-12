08bdab3e859888e330cd82b7f447a6934c4622dc
cheliyan-s <127283399+cheliyan-s@users.noreply.github.com>
2023-08-12 04:09:37 -0400

    added /weather command using weatherapi

--- cogs/responses.py
+++ cogs/responses.py
@@ -1,7 +1,9 @@
 import discord
 import random
+import requests
 from discord import app_commands
 from discord.ext import commands
+from config import API_KEY
 
 class ResponsesCog(commands.Cog):
     def __init__(self, bot):
@@ -18,6 +20,26 @@ class ResponsesCog(commands.Cog):
         embed = discord.Embed(colour=discord.Colour.dark_blue(), title="List of commands", description="``roll``   |    Roll a dice!")
         await interaction.response.send_message(embed=embed)
     
+    @app_commands.command(name = "weather")
+    @app_commands.describe(city = "Enter a city:")
+    async def weather(self, interaction: discord.Interaction, city:str):
+
+        url = f"http://api.weatherapi.com/v1/current.json?key={API_KEY}&q={city}"  
+        data = requests.get(url).json() 
+        location = data['location']['name']
+        temp_c = data['current']['temp_c']
+        temp_f = data['current']['temp_f']
+        humidity = data['current']["humidity"]
+        wind_kph = data['current']['wind_kph']
+        wind_mph = data['current']['wind_mph']
+        condition = data['current']['condition']['text']
+        image_url = "http:" +data['current']['condition']['icon']
+        embed = discord.Embed(color=discord.Colour.dark_blue(), title = f"Weather in {location}", description=f"The condition is {condition}")
+        embed.add_field(name="Temperature", value=f"C: {temp_c} | F: {temp_f}")
+        embed.add_field(name="Wind", value = f"{wind_kph} km/h | {wind_mph} mph")
+        embed.add_field(name="Humidity", value = f"{humidity}%")
+        embed.set_thumbnail(url = image_url)
+        await interaction.response.send_message(embed=embed)
 async def setup(bot):
     await bot.add_cog(ResponsesCog(bot))
 
