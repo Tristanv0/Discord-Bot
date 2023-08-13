36885668fccf859106dcde15aec7dc0b45ff80e8
cheliyan-s <127283399+cheliyan-s@users.noreply.github.com>
2023-08-12 20:01:49 -0400

    Update responses.py

--- cogs/responses.py
+++ cogs/responses.py
@@ -33,6 +33,7 @@ class ResponsesCog(commands.Cog):
         url = f"http://api.weatherapi.com/v1/current.json?key={API_KEY}&q={city}"  
         data = requests.get(url).json() 
         location = data['location']['name']
+        country = data['location']['country']
         temp_c = data['current']['temp_c']
         temp_f = data['current']['temp_f']
         humidity = data['current']["humidity"]
@@ -40,7 +41,7 @@ class ResponsesCog(commands.Cog):
         wind_mph = data['current']['wind_mph']
         condition = data['current']['condition']['text']
         image_url = "http:" +data['current']['condition']['icon']
-        embed = discord.Embed(color=discord.Colour.dark_blue(), title = f"Weather in {location}", description=f"The condition is {condition}")
+        embed = discord.Embed(color=discord.Colour.dark_blue(), title = f"Weather in {location}, {country}", description=f"The condition is {condition}")
         embed.add_field(name="Temperature", value=f"C: {temp_c} | F: {temp_f}")
         embed.add_field(name="Wind", value=f"{wind_kph} km/h | {wind_mph} mph")
         embed.add_field(name="Humidity", value=f"{humidity}%")
