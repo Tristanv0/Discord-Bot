b25d1f2ea55fe17740f4707c764e7fa665d0a35b
cheliyan-s <127283399+cheliyan-s@users.noreply.github.com>
2023-08-12 20:54:55 -0400

    updated help

--- cogs/responses.py
+++ cogs/responses.py
@@ -17,7 +17,11 @@ class ResponsesCog(commands.Cog):
     @app_commands.command(name="help")
     async def help(self, interaction: discord.Interaction):
         """List of all commands"""
-        embed = discord.Embed(colour=discord.Colour.dark_blue(), title="List of commands", description="``roll``   |    Roll a dice!")
+        embed = discord.Embed(colour=discord.Colour.dark_blue())
+        embed.set_author(name="Command List", icon_url="http://discord.com/assets/7c8f476123d28d103efe381543274c25.png")
+        embed.add_field(name=":speech_balloon: General (2)", value="``/ping`` , ``/weather``", inline=False)
+        embed.add_field(name=":game_die: Games (2)", value="``/blackjack`` , ``/roll``", inline=False)
+        embed.add_field(name=":musical_note: Music (5)", value="``/play`` , ``/pause`` , ``/resume`` , ``/skip`` , ``/queue``", inline=False)
         await interaction.response.send_message(embed=embed)
     
     @app_commands.command(name="ping")
